55fb3255d1b6074fe130417064019f913d0c57df
Rodolfo Sousa <rodolfozsousa@gmail.com>
2022-09-30 01:36:56 -0300

    feat: post categories route

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -6,4 +6,33 @@ async function getCategories(req, res) {
   res.send(categories);
 }
 
-export { getCategories };
+async function postCategories(req, res) {
+  const category = req.body;
+
+  if (!category.name) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    const categories = (
+      await database.query(`SELECT * FROM categories WHERE name = $1`, [
+        category.name,
+      ])
+    ).rows;
+
+    if (categories.length > 0) {
+      return res.sendStatus(409);
+    }
+
+    database.query(`INSERT INTO categories (name) VALUES ($1)`, [
+      category.name,
+    ]);
+
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export { getCategories, postCategories };
--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getCategories } from "../controllers/categoriesController.js";
+import {
+  getCategories,
+  postCategories,
+} from "../controllers/categoriesController.js";
 
 const categories = Router();
 
 categories.get("/categories", getCategories);
+
+categories.post("/categories", postCategories);
 
 export default categories;
